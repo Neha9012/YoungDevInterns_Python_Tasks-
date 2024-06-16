7641607b968e92ea9db125ab062a4274e6d239b2
Neha9012 <115710285+Neha9012@users.noreply.github.com>
2024-06-16 19:31:51 +0500

    Delete Basic Task 2 directory

--- Basic Task 2/scriptsdata_processing.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-# Example data processing function
-def process_data(input_file):
-    data = pd.read_csv(input_file)
-    # Perform data processing steps
-    processed_data = data * 2
-    return processed_data
